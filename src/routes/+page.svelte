fbcfb2952f9319772fffae54dbdaef9c72041fa0
TMK04 <floer.learner.01@gmail.com>
2025-07-20 17:18:58 +0800

    /+page.svelte: add top nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
+	import IconHistory from '~icons/mdi/history';
+	import IconListBoxOutline from '~icons/mdi/list-box-outline';
 	import Search from '$lib/components/Search.svelte';
 </script>
 
-<div class="m-2">
+<div class="m-3">
 	<Search />
+	<!-- svelte-ignore a11y_no_noninteractive_element_to_interactive_role -->
+	<nav class="mb-3" role="tablist">
+		<button class="btn btn-outline btn-sm border-base-300" type="button">
+			<IconHistory /> History
+		</button>
+		<button class="btn btn-outline btn-sm border-base-300" type="button">
+			<IconListBoxOutline /> Pending Exchanges
+		</button>
+	</nav>
 </div>
