9fe0d2e7b8bd029b3d394935cf1cc6533618c378
TMK04 <floer.learner.01@gmail.com>
2025-07-20 12:29:22 +0800

    /signup: init semantics

---
+++ src/routes/(noauth)/signup/+page.svelte
@@ -0,0 +1,29 @@
+<div>
+	<h1>Fashion Forward</h1>
+	<main>
+		<form>
+			<h1>Create an account</h1>
+			<label>
+				<p>Enter your email to sign up for this app</p>
+				<input placeholder="email@domain.com" type="email" />
+			</label>
+			<button type="submit">Continue</button>
+		</form>
+
+		<div>
+			<hr />
+			<span>Or</span>
+		</div>
+
+		<section>
+			<button>Continue with Google</button>
+			<button>Continue with Apple</button>
+		</section>
+
+		<footer>
+			<!-- TODO: design /tos & /privacy -->
+			By clicking continue, you agree to our <a href="/tos">Terms of Service</a> and
+			<a href="/privacy">Privacy Policy</a>
+		</footer>
+	</main>
+</div>
