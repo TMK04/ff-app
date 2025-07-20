e5366c677ccb38f66777681499c63cbdbbcb4b99
TMK04 <floer.learner.01@gmail.com>
2025-07-20 12:25:04 +0800

    /+layout: constrain to phone dimensions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,8 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="bg-black">
+	<div class="mx-auto h-screen max-w-sm overflow-scroll bg-white">
+		{@render children()}
+	</div>
+</div>
