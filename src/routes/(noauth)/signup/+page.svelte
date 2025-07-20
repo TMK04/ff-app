609cfcf18a2ff3f8ec2cf09734a4663da06420f2
TMK04 <floer.learner.01@gmail.com>
2025-07-20 14:39:54 +0800

    signup: style

--- src/routes/(noauth)/signup/+page.svelte
+++ src/routes/(noauth)/signup/+page.svelte
@@ -1,29 +1,51 @@
-<div>
-	<h1>Fashion Forward</h1>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { base } from '$app/paths';
+
+	async function onclick() {
+		goto(`/${base}`);
+	}
+</script>
+
+<div class="mx-8 flex min-h-full flex-col justify-center gap-y-6 text-center">
+	<header class="my-2 text-4xl">Fashion Forward</header>
 	<main>
 		<form>
-			<h1>Create an account</h1>
-			<label>
-				<p>Enter your email to sign up for this app</p>
-				<input placeholder="email@domain.com" type="email" />
+			<label class="mb-4 block">
+				<header class="mb-6">
+					<h1 class="text-large font-bold">Create an account</h1>
+					<p>Enter your email to sign up for this app</p>
+				</header>
+				<input
+					class="input w-full"
+					name="email"
+					autocomplete="email webauthn"
+					placeholder="email@domain.com"
+					type="email"
+				/>
 			</label>
-			<button type="submit">Continue</button>
+			<!-- TODO: auth -->
+			<button class="btn btn-neutral w-full" type="submit" {onclick}>Continue</button>
 		</form>
 
-		<div>
-			<hr />
-			<span>Or</span>
-		</div>
+		<div class="divider my-8 text-gray-400">or</div>
 
-		<section>
-			<button>Continue with Google</button>
-			<button>Continue with Apple</button>
+		<section class="flex flex-wrap gap-2">
+			<!-- TODO: oauth -->
+			<!-- TODO: icons -->
+			<button class="btn grow bg-gray-200 text-black" type="button" {onclick}
+				>Continue with Google</button
+			>
+			<button class="btn grow bg-gray-200 text-black" type="button" {onclick}
+				>Continue with Apple</button
+			>
 		</section>
-
-		<footer>
-			<!-- TODO: design /tos & /privacy -->
-			By clicking continue, you agree to our <a href="/tos">Terms of Service</a> and
-			<a href="/privacy">Privacy Policy</a>
-		</footer>
 	</main>
+	<footer class="text-sm text-gray-400">
+		<!-- TODO: design /tos & /privacy -->
+		By clicking continue, you agree to our
+		<a class="font-bold text-gray-800" href={`${base}/tos`}>Terms of Service</a>
+		and
+		<a class="font-bold text-gray-800" href={`${base}/privacy`}>Privacy Policy</a>
+	</footer>
 </div>
