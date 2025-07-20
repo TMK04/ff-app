47b7bb018eb2b75622312e7f23e366b75f7178df
TMK04 <floer.learner.01@gmail.com>
2025-07-20 17:06:36 +0800

    /+page.svelte: add top search

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import IconMagnify from '~icons/mdi/magnify';
+</script>
+
+<label class="input bg-base-200 border-base-300 mx-3 mb-2">
+	<IconMagnify class="text-base-content/60 [:focus-within>&]:text-base-content text-xl" />
+	<input type="search" class="grow" placeholder="Search" />
+</label>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,7 @@
-<!-- TODO -->
+<script lang="ts">
+	import Search from '$lib/components/Search.svelte';
+</script>
+
+<div class="m-2">
+	<Search />
+</div>
