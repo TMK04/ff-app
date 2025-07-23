65415a59d028d7451b0aa9c7f46c94eab26f83bc
TMK04 <floer.learner.01@gmail.com>
2025-07-24 07:11:15 +0800

    componentize BottomNav, NavTab->BottomNavTab

---
+++ src/lib/components/BottomNav.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import IconAccount from '~icons/mdi/account';
+	import IconAccountKey from '~icons/mdi/account-key';
+	import IconGamepadVariantOutline from '~icons/mdi/gamepad-variant-outline';
+	import IconHanger from '~icons/mdi/hanger';
+	import IconHome from '~icons/mdi/home';
+	import IconLightbulbOnOutline from '~icons/mdi/lightbulb-on-outline';
+
+	import BottomNavTab from '$lib/components/BottomNavTab.svelte';
+	import { me } from '$lib/stores/me';
+</script>
+
+<!-- svelte-ignore a11y_no_noninteractive_element_to_interactive_role -->
+<nav
+	class="border-base-300 grid h-15 auto-cols-fr grid-flow-col grid-rows-1 gap-x-2 border-t p-2"
+	role="tablist"
+>
+	<BottomNavTab Icon={IconHome} path="">Home</BottomNavTab>
+	<BottomNavTab Icon={IconGamepadVariantOutline} path="challenge">Challenge</BottomNavTab>
+	<BottomNavTab Icon={IconHanger} path="explore">Explore</BottomNavTab>
+	<BottomNavTab Icon={IconLightbulbOnOutline} path="learn">Learn</BottomNavTab>
+	<BottomNavTab
+		Icon={$me.auth ? IconAccount : IconAccountKey}
+		path={$me.auth ? 'profile' : 'profile/signup'}>Profile</BottomNavTab
+	>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
-	import IconAccount from '~icons/mdi/account';
-	import IconAccountKey from '~icons/mdi/account-key';
-	import IconGamepadVariantOutline from '~icons/mdi/gamepad-variant-outline';
-	import IconHanger from '~icons/mdi/hanger';
-	import IconHome from '~icons/mdi/home';
-	import IconLightbulbOnOutline from '~icons/mdi/lightbulb-on-outline';
-
-	import NavTab from '$lib/components/NavTab.svelte';
-	import { me } from '$lib/stores/me';
 	import '../app.css';
+
+	import BottomNav from '$lib/components/BottomNav.svelte';
 
 	let { children } = $props();
 </script>
@@ -22,19 +15,6 @@
 		<div class="flex h-0 grow flex-col overflow-scroll pb-8">
 			{@render children()}
 		</div>
-		<!-- svelte-ignore a11y_no_noninteractive_element_to_interactive_role -->
-		<nav
-			class="border-base-300 grid h-15 auto-cols-fr grid-flow-col grid-rows-1 gap-x-2 border-t p-2"
-			role="tablist"
-		>
-			<NavTab Icon={IconHome} path="">Home</NavTab>
-			<NavTab Icon={IconGamepadVariantOutline} path="challenge">Challenge</NavTab>
-			<NavTab Icon={IconHanger} path="explore">Explore</NavTab>
-			<NavTab Icon={IconLightbulbOnOutline} path="learn">Learn</NavTab>
-			<NavTab
-				Icon={$me.auth ? IconAccount : IconAccountKey}
-				path={$me.auth ? 'profile' : 'profile/signup'}>Profile</NavTab
-			>
-		</nav>
+		<BottomNav />
 	</div>
 </div>
