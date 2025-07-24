79d039b96cd6e42561fea60221afccb835610e74
TMK04 <floer.learner.01@gmail.com>
2025-07-24 13:49:26 +0800

    challenge: add Vote,hearts

--- src/routes/challenge/+page.svelte
+++ src/routes/challenge/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import IconHeart from '~icons/mdi/heart';
+
 	import TopNav from '$lib/components/TopNav.svelte';
 </script>
 
@@ -13,4 +15,42 @@
 	</p>
 	<button class="btn btn-primary mx-auto w-max">Sign Up</button>
 </header>
-<main></main>
+<main class="mx-6 flex flex-col items-center">
+	<h3 class="divider mt-12 mb-1 text-lg font-bold underline">Vote</h3>
+	<aside class="custom--hearts">
+		<figure
+			class="bg-base-100/90 flex max-h-full flex-wrap-reverse justify-center gap-1.5 py-4 text-sm"
+		>
+			<figcaption class="grow text-end transition-[flex-grow]">1 of 3 hearts used</figcaption>
+			<span class="stroke-neutral flex w-max justify-center transition-[width]">
+				<IconHeart class="text-red-500" />
+				<IconHeart class="text-red-200" />
+				<IconHeart class="text-red-200" />
+			</span>
+		</figure>
+	</aside>
+	<article></article>
+</main>
+
+<style lang="postcss">
+	@reference "tailwindcss";
+
+	.custom--hearts {
+		@apply sticky top-0 mb-6 h-[4.75rem] w-full;
+		container-type: scroll-state;
+
+		@supports (container-type: scroll-state) {
+			@container scroll-state(stuck: none) {
+				> figure {
+					> figcaption {
+						@apply grow-0;
+					}
+
+					> span {
+						@apply w-full;
+					}
+				}
+			}
+		}
+	}
+</style>
