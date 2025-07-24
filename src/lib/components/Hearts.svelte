795356014bb7a09081ddd6ec49674fc8ee5c7149
TMK04 <floer.learner.01@gmail.com>
2025-07-24 19:25:31 +0800

    componentize Hearts

---
+++ src/lib/components/Hearts.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import IconHeart from '~icons/mdi/heart';
+</script>
+
+<aside class="custom--hearts pointer-events-none sticky top-0 h-[4.75rem] w-full">
+	<figure
+		class="bg-base-100/90 pointer-events-auto flex max-h-full flex-wrap-reverse justify-center gap-1.5 py-4 text-sm"
+	>
+		<figcaption class="grow text-end transition-[flex-grow]">1 of 3 hearts used</figcaption>
+		<span class="stroke-neutral flex w-max justify-center transition-[width]">
+			<IconHeart class="text-red-500" />
+			<IconHeart class="text-red-200" />
+			<IconHeart class="text-red-200" />
+		</span>
+	</figure>
+</aside>
+
+<style>
+	.custom--hearts {
+		container-type: scroll-state;
+
+		@supports (container-type: scroll-state) {
+			@container scroll-state(stuck: none) {
+				> figure {
+					> figcaption {
+						flex-grow: 0;
+					}
+
+					> span {
+						width: 100%;
+					}
+				}
+			}
+		}
+	}
+</style>
--- src/routes/challenge/+page.svelte
+++ src/routes/challenge/+page.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-	import IconHeart from '~icons/mdi/heart';
-
 	import { base } from '$app/paths';
 	import ChallengeEntry from '$lib/components/ChallengeEntry.svelte';
+	import Hearts from '$lib/components/Hearts.svelte';
 	import TopNav from '$lib/components/TopNav.svelte';
 </script>
 
@@ -19,44 +18,10 @@
 </header>
 <main class="mx-6 flex flex-col items-center">
 	<h3 class="divider mt-12 mb-1 text-lg font-bold underline">Vote</h3>
-	<aside class="custom--hearts">
-		<figure
-			class="bg-base-100/90 pointer-events-auto flex max-h-full flex-wrap-reverse justify-center gap-1.5 py-4 text-sm"
-		>
-			<figcaption class="grow text-end transition-[flex-grow]">1 of 3 hearts used</figcaption>
-			<span class="stroke-neutral flex w-max justify-center transition-[width]">
-				<IconHeart class="text-red-500" />
-				<IconHeart class="text-red-200" />
-				<IconHeart class="text-red-200" />
-			</span>
-		</figure>
-	</aside>
+	<Hearts />
 	<ol class="flex w-full flex-wrap gap-8">
 		{#each { length: 10 }}
 			<ChallengeEntry comment_count={4} like_count={21} posted_ago="3mins ago" username="Daniel" />
 		{/each}
 	</ol>
 </main>
-
-<style lang="postcss">
-	@reference "tailwindcss";
-
-	.custom--hearts {
-		@apply pointer-events-none sticky top-0 h-[4.75rem] w-full;
-		container-type: scroll-state;
-
-		@supports (container-type: scroll-state) {
-			@container scroll-state(stuck: none) {
-				> figure {
-					> figcaption {
-						@apply grow-0;
-					}
-
-					> span {
-						@apply w-full;
-					}
-				}
-			}
-		}
-	}
-</style>
