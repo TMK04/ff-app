e8827fde44a5326e9b08a40f59db3d545c8467a7
TMK04 <floer.learner.01@gmail.com>
2025-07-24 13:13:22 +0800

    challenge: add header

--- src/routes/challenge/+page.svelte
+++ src/routes/challenge/+page.svelte
@@ -3,3 +3,14 @@
 </script>
 
 <TopNav>Sustainability Fashion Challenge</TopNav>
+<header class="mx-6 flex flex-col justify-center">
+	<h2 class="mx-6 mb-3 text-center">
+		This Month's Challenge Theme is<br /><span class="text-xl font-bold">Summer is in the air</span>
+	</h2>
+	<div class="skeleton mx-6 mb-4 aspect-[3/4]"></div>
+	<p class="mb-6 text-center">
+		This Month's Prize: $100 Levi's Gift Card<br />There will be 1 winner for every 250 participants
+	</p>
+	<button class="btn btn-primary mx-auto w-max">Sign Up</button>
+</header>
+<main></main>
