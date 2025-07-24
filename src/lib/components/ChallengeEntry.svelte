0be9074027ed42f40a5be22e7a9f6c6710341c71
TMK04 <floer.learner.01@gmail.com>
2025-07-24 15:28:12 +0800

    ChallengeEntry: make like btn checkbox

--- src/lib/components/ChallengeEntry.svelte
+++ src/lib/components/ChallengeEntry.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import IconCommentMultipleOutline from '~icons/mdi/comment-multiple-outline';
 	import IconDotsHorizontal from '~icons/mdi/dots-horizontal';
-	import IconHeartOutline from '~icons/mdi/heart-outline';
+	import IconHeart from '~icons/mdi/heart';
 </script>
 
 <li class="flex w-xs grow flex-col">
@@ -14,8 +14,14 @@
 		<button class="btn btn-ghost" type="button"><IconDotsHorizontal /></button>
 	</header>
 	<div class="skeleton ms-12 mb-3 h-64 w-48"></div>
-	<footer class="ms-10 flex gap-4">
-		<button class="flex items-center gap-1"><IconHeartOutline /><b>21 likes</b></button>
+	<footer class="ms-12 flex gap-4">
+		<label class="flex items-center gap-1">
+			<input name="like" class="hidden" type="checkbox" />
+			<IconHeart
+				class="stroke-neutral text-neutral-content stroke-2 transition-colors [:checked+&]:text-red-500"
+			/>
+			<b>21 likes</b>
+		</label>
 		<button class="flex items-center gap-1">
 			<IconCommentMultipleOutline /><b>4 comments</b>
 		</button>
