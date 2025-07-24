63cd150c8d61add36f3ac2e0a485fe850d86920e
TMK04 <floer.learner.01@gmail.com>
2025-07-24 16:10:24 +0800

    /challenge/signup: add Preview

--- src/routes/challenge/signup/+page.svelte
+++ src/routes/challenge/signup/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
+	import ChallengeEntry from '$lib/components/ChallengeEntry.svelte';
 	import TopNav from '$lib/components/TopNav.svelte';
 </script>
 
 <TopNav>Sign Up for Challenge</TopNav>
+
+<main class="mx-6">
+	<section>
+		<h2 class="divider mb-8 text-center text-lg font-bold underline">Preview</h2>
+		<ChallengeEntry posted_ago="just now" />
+	</section>
+</main>
