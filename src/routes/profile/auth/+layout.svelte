c11b0a72fa5cc0fc42f644095269e07bf170ecf3
TMK04 <floer.learner.01@gmail.com>
2025-08-11 01:41:32 +0800

    /profile/auth: move wrapper & header to +layout

---
+++ src/routes/profile/auth/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	type TProps = {
+		children: Snippet;
+	};
+	const { children }: TProps = $props();
+</script>
+
+<div class="mx-8 mt-12 flex grow flex-col justify-center gap-y-6 text-center">
+	<h1 class="font-cursive mb-4 text-5xl">Fashion Forward</h1>
+	{@render children()}
+</div>
--- src/routes/profile/auth/+page.svelte
+++ src/routes/profile/auth/+page.svelte
@@ -24,44 +24,41 @@
 	};
 </script>
 
-<div class="mx-8 mt-12 flex grow flex-col justify-center gap-y-6 text-center">
-	<h1 class="font-cursive mb-4 text-5xl">Fashion Forward</h1>
-	<main>
-		<form {onsubmit}>
-			<label class="block">
-				<header class="mb-4">
-					<h2 class="text-large font-bold">Enter your email to continue</h2>
-				</header>
-				<input
-					name="email"
-					class="input validator w-full"
-					autocomplete="email webauthn"
-					placeholder="email@domain.com"
-					required
-					type="email"
-					value="janedoe@mail.com"
-				/>
-			</label>
-			<!-- TODO: auth -->
-			<button class="btn btn-neutral mt-3 w-full" type="submit">Continue</button>
-		</form>
+<main>
+	<form {onsubmit}>
+		<label class="block">
+			<header class="mb-4">
+				<h2 class="text-large font-bold">Enter your email to continue</h2>
+			</header>
+			<input
+				name="email"
+				class="input validator w-full"
+				autocomplete="email webauthn"
+				placeholder="email@domain.com"
+				required
+				type="email"
+				value="janedoe@mail.com"
+			/>
+		</label>
+		<!-- TODO: auth -->
+		<button class="btn btn-neutral mt-3 w-full" type="submit">Continue</button>
+	</form>
 
-		<div class="divider text-base-content/50 my-8">or</div>
+	<div class="divider text-base-content/50 my-8">or</div>
 
-		<section class="flex flex-wrap gap-2">
-			<!-- TODO: oauth -->
-			<button class="btn text-base-content bg-base-200 grow" disabled type="button">
-				<IconGoogleIcon />Continue with Google</button
-			>
-			<button class="btn text-base-content bg-base-200 grow" disabled type="button">
-				<IconApple />Continue with Apple</button
-			>
-		</section>
-	</main>
-	<footer class="text-base-content/50 text-sm">
-		By clicking continue, you agree to our
-		<a class="link-neutral link-hover font-bold" href={`${base}/tos`}>Terms of Service</a>
-		and
-		<a class="link-neutral link-hover font-bold" href={`${base}/privacy`}>Privacy Policy</a>
-	</footer>
-</div>
+	<section class="flex flex-wrap gap-2">
+		<!-- TODO: oauth -->
+		<button class="btn text-base-content bg-base-200 grow" disabled type="button">
+			<IconGoogleIcon />Continue with Google</button
+		>
+		<button class="btn text-base-content bg-base-200 grow" disabled type="button">
+			<IconApple />Continue with Apple</button
+		>
+	</section>
+</main>
+<footer class="text-base-content/50 text-sm">
+	By clicking continue, you agree to our
+	<a class="link-neutral link-hover font-bold" href={`${base}/tos`}>Terms of Service</a>
+	and
+	<a class="link-neutral link-hover font-bold" href={`${base}/privacy`}>Privacy Policy</a>
+</footer>
