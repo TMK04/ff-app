8ea06c19768918c49349dd93a0f58ae76fec1dff
TMK04 <floer.learner.01@gmail.com>
2025-08-11 01:23:23 +0800

    add delivery summary

---
+++ src/routes/delivery/[id]/summary/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+</script>
+
+<div class="mx-3 mt-6">
+	<header class="mb-6 text-xl font-bold">
+		<h2>Your Chosen Delivery Options on (13/7/2025)</h2>
+	</header>
+	<main>
+		<div class="flex h-max flex-col items-start p-2">
+			<p class="mb-1 text-start font-bold">F2F Meet Up</p>
+			<p class="ms-1 text-start text-sm">Cost: Free — No charges</p>
+		</div>
+		<button class="btn btn-primary mt-6 w-full" type="submit">Track your order</button>
+		<button class="btn btn-primary mt-3 w-full" type="submit">
+			Confirm that Clothing is received
+		</button>
+	</main>
+</div>
